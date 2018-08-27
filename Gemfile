0095326e2baa27610e635b4dde54546a1413b3ed
Juan Carlos Santiago <krlos013@gmail.com>
2018-08-27 17:45:43 -0500

    documentos

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 gem 'devise'
+gem 'rest-client', '>= 2.0.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/controllers/api/v1/get_user_controller.rb
+++ app/controllers/api/v1/get_user_controller.rb
@@ -1,17 +1,41 @@
 class Api::V1::GetUserController <ApplicationController
-require 'uri'
-require 'net/http'
-
+	require 'uri'
+	require 'net/http'
+	require 'base64'
+	require 'open-uri'
+	require 'rest-client'
+	
 skip_before_action :verify_authenticity_token
 	def index
       # organizacion = params[:organizacion]
-       url = "https://intranet-10d95.firebaseio.com/usuarios.json"
+       url = "https://intranet-10d95.firebaseio.com/usuarios.json?print=pretty"
     	 response = RestClient::Request.execute(
 				method: :get,
-       	url: url,
-			# 	headers: {"Content-Type" => "application/json"}
-       )
-  	render json: { :mensaje => JSON.parse(response.body) }
+       			url: url,
+	   )
+
+	   valor = response.body
+	   
+	   new_hash = JSON.parse(valor)
+	   e = new_hash.keys.map do |w|
+			new_hash[w]
+	   end
+	   puts e
+
+	   t = e.map do |r|
+		{
+			:id => r['uid'],
+			:name => r['nombre']
+		}
+		
+	   end
+
+	
+  	   render :json => t
 		#render json: { :mensaje => "JSON.parse(response.body)" }
 	end
+
 end
+
+
+
